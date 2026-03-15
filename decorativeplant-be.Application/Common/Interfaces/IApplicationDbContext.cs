c05554c8f4d44958f3786063811ff32003a2f292
BuiMinhWin <167419186+BuiMinhWin@users.noreply.github.com>
2026-03-15 15:48:32 +0700

    refactor: refactor dbcontext

--- decorativeplant-be.Application/Common/Interfaces/IApplicationDbContext.cs
+++ decorativeplant-be.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -8,9 +8,7 @@ namespace decorativeplant_be.Application.Common.Interfaces;
 /// </summary>
 public interface IApplicationDbContext
 {
-    // Module 1: Identity & Access
-    DbSet<UserAccount> UserAccounts { get; }
-
+   
     // Module 1: Identity & Access
     DbSet<UserAccount> UserAccounts { get; }
     DbSet<UserSubscription> UserSubscriptions { get; }
